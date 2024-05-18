2d2f67b970180fe13a58099b164e1adf180dcd12
carlos <carlosbonfim722@gmail.com>
2024-05-18 20:37:59 -0300

    version1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import GaleriaDeImagens from './components/GaleriaDeImagens'
+import './App.css'
+
+function App() {
+
+  return (
+    <GaleriaDeImagens/>
+  )
+}
+
+export default App
